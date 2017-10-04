7f1700ffd934b92b0590fd3faeac59316a17e397
DCdesign-is <egoamstro379@gmail.com>
2017-10-04 11:15:25 -0500

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,8 +1,9 @@
 /*
 Forms Validate 
 */
-var ck_name = /^[ÑA-Zña-z ]{3,25}$/;
-var ck_last_name = /^[ÑA-Zña-z ]{3,25}$/;
+
+var ck_name = /^[a-zA-ZñÑ\s\W]{3,25}$/;
+var ck_last_name = /^[a-zA-ZñÑ\s\W]{3,25}$/;
 var ck_email = /^(([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}){1,25})+([;.](([a-zA-Z0-9_\-\.]+)@{[a-zA-Z0-9_\-\.]+0\.([a-zA-Z]{2,5}){1,25})+)*$/;
 var ck_phone = /^\d{7,15}$/;
 
@@ -24,7 +25,7 @@ function validate(WebToLeadForm){
 		return false;
 	}
 	if(!ck_name.test(WebToLeadForm.first_name.value)){
-		document.getElementById("name_loc").innerHTML= "Revise este campo !";
+		document.getElementById("name_loc").innerHTML= "Revise este campo!";
 		document.WebToLeadForm.first_name.focus();
 		return false;
 	}	
@@ -36,7 +37,7 @@ function validate(WebToLeadForm){
 		return false;
 	}
 	if (!ck_last_name.test(WebToLeadForm.last_name.value)){
-		document.getElementById("last_name_loc").innerHTML = "Revise este campo !";
+		document.getElementById("last_name_loc").innerHTML = "Revise este campo!";
 		document.WebToLeadForm.last_name.focus();
 		return false;
 	}
@@ -49,20 +50,20 @@ function validate(WebToLeadForm){
 		return false;
 	}	
 	if(!ck_email.test(WebToLeadForm.email1.value)){
-		document.getElementById("email_loc").innerHTML = "Revise este campo !";
+		document.getElementById("email_loc").innerHTML = "Revise este campo!";
 		document.WebToLeadForm.email1.focus();
 		return false;
 	}
 
 
-	//Input Número de Teléfono
+	//Input NÃºmero de TelÃ©fono
 	if(WebToLeadForm.phone_mobile.value == ""){
 		document.getElementById("phone_loc").innerHTML = "Debe completar este campo";
 		document.WebToLeadForm.phone_mobile.focus();
 		return false;
 	}		
 	if(!ck_phone.test(WebToLeadForm.phone_mobile.value) || isNaN(WebToLeadForm.phone_mobile.value)){
-		document.getElementById("phone_loc").innerHTML = "Revise este campo !";
+		document.getElementById("phone_loc").innerHTML = "Revise este campo!";
 		document.WebToLeadForm.phone_mobile.focus();
 		return false;
 	}
@@ -84,7 +85,7 @@ function validate(WebToLeadForm){
 	}	
 
 
-	// Checkbox Términos y Condiciones
+	// Checkbox TÃ©rminos y Condiciones
 	if(!WebToLeadForm.checkbox.checked){
 		document.getElementById("terms_loc").innerHTML = "Debe aceptar los termino de condiciones para continuar";
 		document.WebToLeadForm.checkbox.focus();
